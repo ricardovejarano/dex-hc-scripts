df8198ad4bdfae6039bda3e328eab49449b9dc28
Arnold jair Jimenez Vargas <arnold.jimenez@areamovil.com.co>
2020-01-29 11:19:52 -0500

    RoofTilesInfo

--- 6-DML_ROOF.sql
+++ 6-DML_ROOF.sql
@@ -2,4 +2,27 @@ UPDATE Step SET Code = 'rt_1_aguas' WHERE Code = 'rt_1_wathers';
 UPDATE Field SET Name = 'aguas' WHERE Name = 'wathers';
 
 
-select * from field;
+--RoofTilesInfo
+IF NOT EXISTS (SELECT * 
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE TABLE_SCHEMA = 'dbo' 
+                 AND  TABLE_NAME = 'RoofTilesInfo')
+BEGIN
+	CREATE TABLE [dbo].[RoofTilesInfo](
+		[Id] [int] IDENTITY(1,1),
+		[Sku] [nvarchar](100) NOT NULL,
+		[Product] [nvarchar](100) NOT NULL UNIQUE,
+		[Thickness] [float] default 0,
+		[Length] [float] default 0,
+		[Width] [float] default 0,
+		[Area] [float] default 0,
+		[UsefullArea] [float] default 0,
+		[FasteningsPerTile] [int] default 0,
+		[FasteningSKu] [nvarchar](100),
+		[Trestle] [nvarchar](100),
+		[UE] [int] default 0,
+        [EAN] [nvarchar](100),
+	 	CONSTRAINT PK_RoofTilesInfo PRIMARY KEY (ID) 
+	)
+END
+GO
